b97326152f429ab0056e3f77c775c8041e81dc45
Xavier Fenton <xavierfenton99@gmail.com>
2023-10-04 19:33:14 +1300

    commit for push

---
+++ assets/headerlink.js
@@ -0,0 +1,22 @@
+// const headerMain = () => {
+//   let getId = document.getElementById('Orswo').innerHTML
+//   console.log(getId)
+
+//   // document.addEventListener('click', (e) => {
+//   //   console.log(e.target.innerHTML) // logs `true`
+//   //   if (getId === 'Orswo') {
+//   //     let getId = document.getElementById('shopify-section-main-header')
+//   //     let getParentDiv = getId.parentNode
+
+//   //     getId.innerHTML += '<ul><a>Shop</a><a>Contact<a/></ul>'
+//   //     console.log(getParentDiv)
+//   //     console.log(true)
+//   //   } else {
+//   //     console.log('nothing')
+//   //   }
+//   // })
+//   // let liq = '{{ linklists.main-menu.links }}'
+//   // console.log('{{ linklists.main-menu.links }}')
+// }
+
+// headerMain()
