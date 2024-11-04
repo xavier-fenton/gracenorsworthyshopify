3b591bfb77dbcadbc6e232cb03380242ab4958fa
Xavier Fenton <xavierfenton99@gmail.com>
2024-11-04 13:33:09 +1300

    update conditions 'all' to be initially checked

--- assets/type-filter.js
+++ assets/type-filter.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
     // When a filter option is selected
     $('input[name="type-filter"]').change(function() {
       const selectedType = $(this).attr('id');
-        console.log(selectedType);
       // Show all products if "all" is selected
       if (selectedType === 'all') {
         $('.product-card').show();
