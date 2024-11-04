fde8f18c0f58381b65b7208ea6a0458a721bbad0
Xavier Fenton <xavierfenton99@gmail.com>
2024-11-04 17:43:05 +1300

    have initial image show then cycle. Some additional clean up code also in this commit

--- assets/display-images.js
+++ assets/display-images.js
@@ -1,32 +1,29 @@
-console.log("Hello from display images")
 
 function randomIntFromInterval(min, max) { // min and max included 
         return Math.floor(Math.random() * (max - min + 1) + min);
 }
       
       
-$(document).ready(function() {
+$(document).ready(() => {
     // Access the data attribute
     const imagesString = $('.collect-images').data('images');
-    console.log(imagesString);
     // Step 1: Split the string at each occurrence of "files/"
     let imagesArray = imagesString.split("files/");
 
     // Step 2: Add "files/" back to each element (ignoring the first empty element)
     imagesArray = imagesArray
-    .filter(Boolean) // Remove any empty elements (e.g., at the start)
+    .filter(Boolean)
     .map(image => "files/" + image); // Add "files/" prefix back
 
 
-
     function imageSource(images) {
-       return images[randomIntFromInterval(0, images.length)]
+       return images[randomIntFromInterval(0, images.length - 1)]
     }
     
     setInterval(() => {
         $("#featured-image").attr("src", `/cdn/shop/${imageSource(imagesArray)}`);
         
-    }, 5000);
+    }, 3500);
 
 
   });
