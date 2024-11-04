1d07ea43a61c4fb58636ed4b79177d9586cc151c
Xavier Fenton <xavierfenton99@gmail.com>
2024-11-04 13:24:05 +1300

    filter by product type, using jquery

---
+++ assets/type-filter.js
@@ -0,0 +1,20 @@
+console.log("hello");
+
+
+$(document).ready(function() {
+    // When a filter option is selected
+    $('input[name="type-filter"]').change(function() {
+      const selectedType = $(this).attr('id');
+        console.log(selectedType);
+      // Show all products if "all" is selected
+      if (selectedType === 'all') {
+        $('.product-card').show();
+      } else {
+        // Hide all products
+        $('.product-card').hide();
+  
+        // Show only the products that match the selected type
+        $('.product-card[data-type="' + selectedType + '"]').show();
+      }
+    });
+  });
