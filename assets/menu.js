bbb9e97c7ab73615e05c41bffb2b9f5546d4cc2f
Xavier Fenton <xavierfenton99@gmail.com>
2025-10-18 20:55:52 +1300

    switch menu button

--- assets/menu.js
+++ assets/menu.js
@@ -1,3 +1,11 @@
 function toggleSlideUp() {
   document.getElementById('mobile-menu').classList.toggle('translate-y-full');
+  let menuButton = document.getElementById('menu')
+  if(menuButton.innerHTML == "close")
+  {
+    menuButton.innerHTML = "menu"
+  } else
+  {
+    menuButton.innerHTML = "close"
+  }
 }
