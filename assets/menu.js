35e78d4bbb2591f99e1709209da206a2e692d89f
Xavier Fenton <xavierfenton99@gmail.com>
2025-10-18 22:03:54 +1300

    info tab

--- assets/menu.js
+++ assets/menu.js
@@ -1,5 +1,14 @@
+/**
+  Todo:
+    Close one tab if opening another eg: cart open and user clicks info, close cart so info opens
+ */
+
+
+
 function toggleSlideUp() {
   document.getElementById('mobile-menu').classList.toggle('translate-y-full');
+  document.getElementById('mobile-menu').classList.toggle('active');
+
   let menuButton = document.getElementById('menu')
   if(menuButton.innerHTML == "close")
   {
@@ -8,4 +17,14 @@ function toggleSlideUp() {
   {
     menuButton.innerHTML = "close"
   }
+
+
+}
+
+function togglePageMenu() {
+  document.getElementById("infoTab").classList.toggle('hidden')
+  document.getElementById("infoTab").classList.toggle('active')
+
+
+  
 }
