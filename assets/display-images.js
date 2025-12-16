61bf29c199744387c6ca588b54c6114b96755918
Xavier Fenton <xavierfenton99@gmail.com>
2025-12-16 14:37:12 +1300

    update image script

--- assets/display-images.js
+++ assets/display-images.js
@@ -1,29 +1,31 @@
 
+
 function randomIntFromInterval(min, max) { // min and max included 
         return Math.floor(Math.random() * (max - min + 1) + min);
 }
-      
-      
-$(document).ready(() => {
-    // Access the data attribute
-    const imagesString = $('.collect-images').data('images');
-    // Step 1: Split the string at each occurrence of "files/"
-    let imagesArray = imagesString.split("files/");
 
-    // Step 2: Add "files/" back to each element (ignoring the first empty element)
-    imagesArray = imagesArray
-    .filter(Boolean)
-    .map(image => "files/" + image); // Add "files/" prefix back
-
-
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const imagesString = document.querySelector('#collect-images');
+    let imagesArray = imagesString.dataset.images.split("files/").filter(Boolean);
+    
     function imageSource(images) {
-       return images[randomIntFromInterval(0, images.length - 1)]
+        console.log(images[randomIntFromInterval(0, images.length - 1)]);
+        
+        return images[randomIntFromInterval(0, images.length - 1)]
     }
     
+    document.querySelector("#featured-image").setAttribute("src", `/cdn/shop/files/${imageSource(imagesArray)}`);   
+    
     setInterval(() => {
-        $("#featured-image").attr("src", `/cdn/shop/${imageSource(imagesArray)}`);
+         document.querySelector("#featured-image").classList.remove("blur-xs")
+         document.querySelector("#featured-image").setAttribute("src", `/cdn/shop/files/${imageSource(imagesArray)}`);
+         
+     }, 3500);
+     setInterval(() => {
+        document.querySelector("#featured-image").classList.remove("blur-xs")
+
         
-    }, 3500);
-
-
-  });
+    }, 1000);
+    
+});
